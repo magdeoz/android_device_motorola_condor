898f7b2264336ded0229764494cb92518946934a
magdeoz <hackmagodeoz@gmail.com>
2016-02-19 12:05:06 -0600

    add SD support

--- mr_init_devices.c
+++ mr_init_devices.c
@@ -5,15 +5,10 @@
 // MultiROM needs to init framebuffer, mmc blocks, input devices,
 // some ADB-related stuff and USB drives, if OTG is supported
 // You can use * at the end to init this folder and all its subfolders
-// The init for Motorola Moto E .
-// TO find out for your device "find /sys | grep uevent | grep *the_thingIwantToInit*"
-
 const char *mr_init_devices[] =
 {
-    // framebuffer device
     "/sys/class/graphics/fb0",
 
-    // storage devices
     "/sys/block/mmcblk0",
     "/sys/devices/msm_sdcc.1",
     "/sys/devices/msm_sdcc.1/mmc_host/mmc0",
@@ -25,17 +20,20 @@ const char *mr_init_devices[] =
     "/sys/module/mmc_core",
     "/sys/module/mmcblk",
 
-    // input
     "/sys/devices/gpio_keys.75/input*",
+    "/sys/class/input/event4", //touch
     "/sys/devices/virtual/input*",
-    "/sys/devices/f9923000.i2c/i2c-1/1-0020/input*",
     "/sys/devices/virtual/misc/uinput",
 
-    // adb
+    // for adb
     "/sys/devices/virtual/tty/ptmx",
     "/sys/devices/virtual/misc/android_adb",
     "/sys/devices/virtual/android_usb/android0/f_adb",
     "/sys/bus/usb",
+
+    // sdcard
+    "/sys/block/mmcblk1",
+    "/sys/devices/msm_sdcc.2/mmc_host*",
 
     NULL
 };
