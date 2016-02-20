1ff28dfbe70ed78023548aff7583d7121d449b47
magdeoz <hackmagodeoz@gmail.com>
2016-02-20 13:53:14 -0600

    test2

---
+++ mr_init_devices.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+
+// These are paths to folders in /sys which contain "uevent" file
+// need to init this device.
+// MultiROM needs to init framebuffer, mmc blocks, input devices,
+// some ADB-related stuff and USB drives, if OTG is supported
+// You can use * at the end to init this folder and all its subfolders
+const char *mr_init_devices[] =
+{
+    "/sys/class/graphics/fb0",
+
+    "/sys/block/mmcblk0",
+    "/sys/devices/msm_sdcc.1",
+    "/sys/devices/msm_sdcc.1/mmc_host/mmc0",
+    "/sys/devices/msm_sdcc.1/mmc_host/mmc0/mmc0:0001",
+    "/sys/devices/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0",
+    "/sys/devices/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p31", // boot
+    "/sys/devices/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p32", //cache
+    "/sys/devices/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p1",  //modem
+    "/sys/devices/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p29", //persist
+    "/sys/devices/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p34", //system
+    "/sys/devices/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p36", //userdata
+    "/sys/bus/mmc",
+    "/sys/bus/mmc/drivers/mmcblk",
+    "/sys/module/mmc_core",
+    "/sys/module/mmcblk",
+
+    // input
+    "/sys/devices/gpio_keys.75/input*",
+    "/sys/devices/virtual/input*",
+    "/sys/devices/f9923000.i2c/i2c-1/1-0020/input*",
+    "/sys/devices/virtual/misc/uinput",
+
+    // for adb
+    "/sys/devices/virtual/tty/ptmx",
+    "/sys/devices/virtual/misc/android_adb",
+    "/sys/devices/virtual/android_usb/android0/f_adb",
+    "/sys/bus/usb",
+
+    // sdcard
+    "/sys/block/mmcblk1",
+    "/sys/devices/msm_sdcc.2/mmc_host*",
+
+    NULL
+};
+
